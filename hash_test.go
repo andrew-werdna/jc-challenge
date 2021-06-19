629aa8eba3a84dce530c1c52ae00982833f0d885
altair <andrew-werdna@users.noreply.github.com>
2021-06-19 15:41:26 -0500

    got first failing test

--- hash_test.go
+++ hash_test.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
+	"strconv"
 	"testing"
 )
 
@@ -10,15 +14,38 @@ func TestHashCreator(t *testing.T) {
 		w http.ResponseWriter
 		r *http.Request
 	}
+
+	hpReq, _ := http.NewRequest(http.MethodPost, "http://localhost:8080/hash", bytes.NewReader([]byte("password=angryMonkey")))
+	hpArgs := args{
+		w: httptest.NewRecorder(),
+		r: hpReq,
+	}
+	hpArgs.r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
 	tests := []struct {
-		name string
-		args args
+		name         string
+		args         args
+		shouldReturn int
 	}{
-		// TODO: Add test cases.
+		{
+			name:         "should return a simple number",
+			args:         hpArgs,
+			shouldReturn: 1,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			HashCreator(tt.args.w, tt.args.r)
+			recorder, _ := tt.args.w.(*httptest.ResponseRecorder)
+			resp := recorder.Result()
+			body, _ := ioutil.ReadAll(resp.Body)
+			val, err := strconv.Atoi(string(body))
+			if err != nil {
+				t.Error("did not return a number")
+			}
+			if val != tt.shouldReturn {
+				t.Error("returned unexpected number")
+			}
 		})
 	}
 }
