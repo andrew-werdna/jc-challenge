2ed2cf9858f8f9b6e999f687d0723e74a98af03e
altair <andrew-werdna@users.noreply.github.com>
2021-06-19 11:29:37 -0500

    start unit test scaffolding

--- hash_test.go
+++ hash_test.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestHashCreator(t *testing.T) {
+	type args struct {
+		w http.ResponseWriter
+		r *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			HashCreator(tt.args.w, tt.args.r)
+		})
+	}
+}
+
+func TestHashRetriever(t *testing.T) {
+	type args struct {
+		w http.ResponseWriter
+		r *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			HashRetriever(tt.args.w, tt.args.r)
+		})
+	}
+}
